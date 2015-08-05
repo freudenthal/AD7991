ce59281bffc774e4e95687aacb7232372940e161
freudenthal <jfreudenthal@hindsinstruments.com>
2015-08-05 11:26:44 -0700

    Fleshing out the code some more.

--- AD7991.cpp
+++ AD7991.cpp
@@ -1,12 +1,12 @@
 #include "Arduino.h"
 #include "i2c_t3.h"
-#define MaxOfChannels 4
+#define MaxChannels 4
 #define InternalReference 2.5f
 #define DefaultAddress 40
 using namespace std;
-enum sampleDelayMode{Unknown, On, Off};
-enum i2CFilterMode{Unknown, On, Off};
-enum referenceMode{Unknown, Internal, External};
+enum class sampleDelayMode{Unknown, On, Off};
+enum class i2CFilterMode{Unknown, On, Off};
+enum class referenceMode{Unknown, Internal, External};
 class AD7991
 {
 	public:
@@ -31,9 +31,9 @@ class AD7991
 		float getVRefExt(); 
 	private:
 		uint8_t Address;
-		bool Power[NumberOfChannels];
-		float Voltage[NumberOfChannels];
-		int VoltageInt[NumberOfChannels];
+		bool Power[MaxChannels];
+		float Voltage[MaxChannels];
+		int VoltageInt[MaxChannels];
 		referenceMode ReferenceMode;
 		i2CFilterMode I2CFilterMode;
 		sampleDelayMode SampleDelayMode;
@@ -45,9 +45,9 @@ class AD7991
 };
 AD7991::AD7991()
 {
-	I2CFilterMode = i2CFilterMode.Unknown;
-	ReferenceMode = referenceMode.Unknown;
-	SampleDelayMode = sampleDelayMode.Unknown;
+	I2CFilterMode = i2CFilterMode::Unknown;
+	ReferenceMode = referenceMode::Unknown;
+	SampleDelayMode = sampleDelayMode::Unknown;
 	Address = DefaultAddress;
 	for (int Index = 0; Index < NumberOfChannels; Index++)
 	{
@@ -69,7 +69,17 @@ void AD7991::setAddress(uint8_t address)
 }
 bool AD7991::isConnected()
 {
-	
+	int Status = 5;
+	Wire.beginTransmission(Address);
+	Status = Wire.endTransmission();
+	if (Status == 0)
+	{
+		return true;
+	}	
+	else
+	{
+		return false;
+	}
 }
 float* AD7991::getVoltage(int* Channels, size_t NumberOfChannels)
 {
@@ -127,4 +137,74 @@ void AD7991::setVRefExt(float VRef)
 float AD7991::getVRefExt()
 {
 	return VRefExt;
+}
+void AD7991::ResetCommandByte()
+{
+	CommandByte = 0;
+}
+void AD7991::SetCommandByteAddress(uint8_t DACAddress)
+{
+	CommandByte = DACAddress;
+}
+void AD7991::SetCommandByteCommand(commandMode Command)
+{
+	CommandByte = ( (uint8_t)Command ) << 3;
+}
+void AD7991::SendI2C()
+{
+  bool MoveOn = false;
+  const int MaxAttempts = 16;
+  int CurrentAttempt = 0;
+  int SendSuccess = 5;
+  while (!MoveOn)
+  {
+	Wire.beginTransmission(Address);
+	Wire.write(CommandByte);
+	Wire.write(MSBByte);
+	Wire.write(LSBByte);
+	SendSuccess = Wire.endTransmission(I2C_STOP,I2CTimeout);
+    if(SendSuccess != 0)
+    {
+      Wire.getError();
+      Wire.finish();
+      Wire.resetBus();
+      CurrentAttempt++;
+      if (CurrentAttempt > MaxAttempts)
+      {
+        MoveOn = true;
+        Serial.println("Unrecoverable I2C transmission error.");
+      }
+    }
+    else
+    {
+      MoveOn = true;
+    }
+  }
+}
+void AD7991::RecieveI2C(byte Address, int NumberOfBytes)
+{
+  if(NumberOfBytes > I2CRecieveBufferSize)
+  {
+    NumberOfBytes = I2CRecieveBufferSize;
+  }
+  if(NumberOfBytes > 0)
+  {
+    digitalWrite(LEDPin,HIGH);
+    //Wire.beginTransmission(Address);
+    //Wire.endTransmission(I2C_NOSTOP,I2CTimeout);
+    Wire.requestFrom(Address, NumberOfBytes, I2C_STOP,I2CTimeout);
+    int BytesToCollect = Wire.available();
+    for (int RecieveByteNumber=0; RecieveByteNumber < BytesToCollect; RecieveByteNumber++)
+    {
+      if(RecieveByteNumber<I2CRecieveBufferSize)
+      {
+        I2CRecieveBuffer[RecieveByteNumber] = Wire.readByte();
+      }
+      else
+      {
+        Wire.readByte();
+      }
+    }
+    digitalWrite(LEDPin,LOW);
+  }
 }
